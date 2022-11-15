f7d65bd5ca1a0bb6787de23ec6fc140a80f7984d
katrepalin <trepalin.konstantin@gmail.com>
2022-11-16 02:17:29 +0400

    axios request for next generation added, '/api/v1.0/generation' route added, .gitingnore modified

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from game_of_life import GameOfLife
 
 app = Flask(__name__)
@@ -13,10 +13,18 @@ def index():
 @app.route('/live')
 def live():
     game = GameOfLife()
-    if game.counter > 0:
-        game.form_new_generation()
     game.counter += 1
     return render_template('live.html', game=game)
+
+
+@app.route('/api/v1.0/generation', methods=['GET'])
+def get_next_generation():
+    game = GameOfLife()
+    game.form_new_generation()
+    game.counter += 1
+    return jsonify(world=game.world,
+                   old_world=game.old_world,
+                   counter=game.counter)
 
 
 if __name__ == '__main__':
